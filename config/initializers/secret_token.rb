8c232d6a80c4f2186fc419a29c3ea404971a2b12
Nancy Decker <nancy@cantinaconsulting.com>
2012-06-12 10:19:46 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SoupExchange::Application.config.secret_token = 'd054846cf910f6085b185f8969a5344ab2bcc47c7b49744f7d7d2072b2f78c12bfcf51047b6b64c0e668d50fbdeb09b4daf69927af1a65fa469e48d26de4b183'
